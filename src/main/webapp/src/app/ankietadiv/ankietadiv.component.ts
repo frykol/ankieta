97ab7c689d422b6147cdd39f78e62d8d1afc206b
frykol <96352817+frykol@users.noreply.github.com>
2022-03-18 16:36:34 +0100

    add form view

--- src/main/webapp/src/app/ankietadiv/ankietadiv.component.ts
+++ src/main/webapp/src/app/ankietadiv/ankietadiv.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AnkietaService } from '../ankieta.service';
 
 @Component({
@@ -11,14 +12,14 @@ export class AnkietadivComponent implements OnInit {
   @Input() qName: String = "";
   @Input() id: number = 0;
 
-  constructor(private ankietaService: AnkietaService) { }
+  constructor(private ankietaService: AnkietaService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   deleteAnkieta(): void{
     this.ankietaService.deleteAnkieta(this.id).subscribe(
-      (response: void) => {console.log(response);}
+      (response: void) => {console.log(response); this.router.navigate(['/add']);}
     )
   }
 
