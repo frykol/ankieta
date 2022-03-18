858ba9db45b54202973e4c14fe4505d83ad5129f
frykol <96352817+frykol@users.noreply.github.com>
2022-03-18 17:42:26 +0100

    added voting function

--- src/main/webapp/src/app/pojankieta/pojankieta.component.ts
+++ src/main/webapp/src/app/pojankieta/pojankieta.component.ts
@@ -1,6 +1,7 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, NgModule, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Ankieta } from '../ankieta';
 import { AnkietaService } from '../ankieta.service';
 
@@ -14,7 +15,7 @@ export class PojankietaComponent implements OnInit {
   ankieta: any = {};
   id: number = 0;
 
-  constructor(private route: ActivatedRoute, private ankietaService: AnkietaService) { }
+  constructor(private route: ActivatedRoute, private ankietaService: AnkietaService, private router: Router) { }
 
 
 
@@ -28,6 +29,30 @@ export class PojankietaComponent implements OnInit {
       (response: Ankieta) => {this.ankieta = response;},
       (error: HttpErrorResponse) => {alert(error);}
     )
+  }
+
+  updateAnkieta(ankieta: Ankieta): void{
+
+    this.ankietaService.updateAnkieta(ankieta).subscribe(
+      (response: Ankieta) => {console.log("es"); this.router.navigate(["/all"]);},
+      (error: HttpErrorResponse) => {alert(error.message);}
+    )
+  }
+
+  getForm(form: NgForm){
+    if(form.value.odp == "a"){
+      this.ankieta.apoints += 1;
+    }
+    else if(form.value.odp == "b"){
+      this.ankieta.bpoints += 1;
+    }
+    else if(form.value.odp == "c"){
+      this.ankieta.cpoints += 1;
+    }
+    else if(form.value.odp == "d"){
+      this.ankieta.dpoints += 1;
+    }
+    this.updateAnkieta(this.ankieta);
   }
 
 }
