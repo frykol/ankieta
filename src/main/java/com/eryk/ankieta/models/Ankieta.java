09b38056276d2a5809922f28320e3d321cc69817
frykol <96352817+frykol@users.noreply.github.com>
2022-03-18 13:10:49 +0100

    small changes

--- src/main/java/com/eryk/ankieta/models/Ankieta.java
+++ src/main/java/com/eryk/ankieta/models/Ankieta.java
@@ -15,12 +15,24 @@ public class Ankieta implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false, updatable = false)
     private Long id;
-    private String QuestionName;
+    private String questionName;
     private String aAnswer;
     private String bAnswer;
     private String cAnswer;
     private String dAnswer;
     private String correctAnswer;
+    private String difficulty;
 
+    public Ankieta() {}
+
+    public Ankieta(String questionName, String aAnswer, String bAnswer, String cAnswer, String dAnswer, String correctAnswer, String difficulty){
+        this.questionName = questionName;
+        this.aAnswer = aAnswer;
+        this.bAnswer = bAnswer;
+        this.cAnswer = cAnswer;
+        this.dAnswer = dAnswer;
+        this.correctAnswer = correctAnswer;
+        this.difficulty = difficulty;
+    }
 
 }
--- src/main/java/com/eryk/ankieta/service/AnkietaService.java
+++ src/main/java/com/eryk/ankieta/service/AnkietaService.java
@@ -3,13 +3,17 @@ package com.eryk.ankieta.service;
 import com.eryk.ankieta.exception.AnkietaNotFoundException;
 import com.eryk.ankieta.models.Ankieta;
 import com.eryk.ankieta.repo.AnkietaRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
 
+@Service
 public class AnkietaService {
     private final AnkietaRepo ankietaRepo;
 
+    @Autowired
     public AnkietaService(AnkietaRepo ankietaRepo) {
         this.ankietaRepo = ankietaRepo;
     }
